f436ae1f84ffb80bafefd9dff3bb1468c524bf22
Rob Halff <rob.halff@gmail.com>
2014-10-15 21:48:58 +0200

    add tests

--- test/learn.js
+++ test/learn.js
@@ -4,6 +4,8 @@ var yaml = require('js-yaml');
 var glob = require('glob');
 var fs = require('fs');
 
+var debug = false;
+
 test('Leason test', function (t) {
 
     glob('./test/fixtures/*.yml', function(err, files) {
@@ -21,6 +23,14 @@ test('Leason test', function (t) {
         );
 
         leason.parse(fixture.data);
+
+        if(debug) {
+          console.log(
+            JSON.stringify(fixture.data, null, 2),
+            JSON.stringify(leason.schema, null, 2),
+            JSON.stringify(fixture.schema, null, 2)
+          );
+        }
 
         t.deepEqual(
           leason.schema,
