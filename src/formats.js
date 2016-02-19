ca6097f5884218797d97d0f0895448877fff31a6
Rob Halff <rob.halff@gmail.com>
2016-02-19 12:27:18 +0100

    use format checks from json-gate

--- src/formats.js
+++ src/formats.js
@@ -1,5 +1,14 @@
 import email from 'rfc822-validate'
+import {formats as jgf} from 'json-gate/lib/formats'
 
 export default {
-  email
+  email,
+  'date-time': (str) => jgf['date-time'].regex.test(str),
+  date: (str) => jgf['date'].regex.test(str),
+  time: (str) => jgf['time'].regex.test(str),
+  phone: (str) => jgf['phone'].func(str),
+  uri: (str) => jgf['uri'].regex.test(str),
+  ipv4: (str) => jgf['uri'].regex.test(str),
+  ipv6: (str) => jgf['uri'].regex.test(str),
+  hostname: (str) => jgf['host-name'].regex.test(str)
 }
