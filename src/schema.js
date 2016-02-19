a6a8b7bbb5c3fa9cc0d35e7f592905bf4f721692
Rob Halff <rob.halff@gmail.com>
2016-02-19 23:40:45 +0100

    clean

--- src/schema.js
+++ src/schema.js
@@ -1,22 +1,5 @@
 /**
- *
  * Schema
- *
- * Loads a json schema and into a hot object
- *
- * The original state is the schema, or after one json is parsed.
- * Both cases lead to the initial state.
- *
- * Each property is hot. The properties are auto visited.
- * Because a next document will arrive with largly the same structure.
- *
- * Each schema should SchemaProperty should keep a state.
- *  - visited Times visited
- *  - values  Keep track of each and every value
- *
- * The values must be kept in a circular array, which length is configurable
- * e.g. you do not want to keep 50 thousand values in memory.
- *
  */
 export default class Schema {
   constructor (schema) {
--- test/fixtures/README.md
+++ test/fixtures/README.md
@@ -1,6 +1,3 @@
 # Test fixtures
 
-Fixtures within a dir contain multiple records within the data section.
-data.yml can also contain options to be used by Leason itself.
-
-schema.yml is the expected learned schema
+Fixtures within a .yml file contain multiple records within the data section.
